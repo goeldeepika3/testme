96abcd6d4bdc4796567d72546732cfc59eae6539
goeldeepika3 <goeldeepika3@gmail.com>
2017-02-09 14:09:14 -0500

    more calls

--- parse.js
+++ parse.js
@@ -2,14 +2,107 @@ const yaml = require('js-yaml');
 const fs = require('fs');
 const dir = require('node-dir');
 const walkSync = require('walk-sync')
+const studyPlanRegex = /.*studyplan.*/gi;
+
+function getFiles(folder) {
+    const files = walkSync(folder);
+    let allFiles = [];
+    files.forEach(function (file) {
+        let fullPath = `${folder}/${file}`;
+        allFiles.push(fullPath);
+    });
+    return allFiles;
+}
+
+exports.list = function (folder) {
+    const files = getFiles(folder);
+    return files;
+};
+exports.read = function (folder) {
+    let data = `location,isFile,currentLandingUrl\r\n`
+    const files = getFiles(folder);
+    files.forEach(function (file) {
+        const location = file;
+        const isFile = fs.statSync(file).isFile();
+        let landingUrl = undefined;
+        let skip = false;
+        if (isFile) {
+            try {
+                var fileContent = yaml.safeLoad(fs.readFileSync(file, 'utf8'));
+                landingUrl = getLandingUrl(fileContent);
+            }
+            catch (err) {
+                skip = true;
+                console.log(`skipped:${file}`);
+            }
+        }
+        if (skip) {
+            return;
+        }
+        data += `${location},${isFile},${landingUrl}\r\n`
+    });
+    return data;
+}
+function getLandingUrl(fileContent) {
+    return fileContent.landingUrl;
+}
+exports.v2 = function (folder) {
+    let data = `location,isFile,currentLandingUrl,defaultLesson,newLandingUrl\r\n`
+    const files = getFiles(folder);
+    files.forEach(function (file) {
+        const location = file;
+        const isFile = fs.statSync(file).isFile();
+        let landingUrl = undefined;
+        let defaultLesson = undefined;
+        let newLandingUrl = undefined;
+        let skip = false;
+        if (file.indexOf('KTP.yaml') !== -1) {
+            console.log('interest')
+        }
+        if (isFile) {
+            try {
+                var fileContent = yaml.safeLoad(fs.readFileSync(file, 'utf8'));
+                landingUrl = getLandingUrl(fileContent);
+                var regexMatchObj = regexMatchv2(fileContent);
+                if (regexMatchObj) {
+                    defaultLesson = regexMatchObj.defaultLesson;
+                    newLandingUrl = regexMatchObj.landingUrl;
+                } else {
+                    defaultLesson = 'N/A';
+                    newLandingUrl = 'N/A';
+                }
+            }
+            catch (err) {
+                skip = true;
+                console.log(`skipped:${file}`)
+            }
+        }
+        if (skip) {
+            return;
+        }
+        data += `${location},${isFile},${landingUrl},${defaultLesson},${newLandingUrl}\r\n`
+    });
+    return data;
+}
+function regexMatchv2(doc) {
+    if (doc.landingUrl) {
+        var matches = doc.landingUrl.match(studyPlanRegex);
+        if (matches && matches.length == 1) {
+            return {
+                defaultLesson: matches[0],
+                landingUrl: '/studyplan'
+            }
+        }
+    }
+};
+
 
 exports.load = function (folder) {
     let data = '';
-    let prefix = process.cwd() + '/atom.web';
     const files = walkSync(folder);
     let yamlFiles = [];
     files.forEach(function (file) {
-        let fullPath = `${prefix}/${file}`;
+        let fullPath = `${folder}/${file}`;
         if (fs.statSync(fullPath).isFile()) {
             yamlFiles.push(fullPath);
         }
@@ -19,7 +112,7 @@ exports.load = function (folder) {
         let updatedDoc = regexMatch(doc);
         if (updatedDoc != null) {
             data += `${file},${updatedDoc.landingUrl},${updatedDoc.studyPlan.defaultLesson}\r\n`;
-        }else{
+        } else {
             console.log('ok');
         }
         //now lets change it
@@ -30,15 +123,14 @@ exports.load = function (folder) {
 }
 
 function regexMatch(doc) {
-    const studyPlan = /.*studyplan.*/gi;
     if (doc.landingUrl) {
-        var matches = doc.landingUrl.match(studyPlan);
+        var matches = doc.landingUrl.match(studyPlanRegex);
         if (matches && matches.length == 1) {
             if (!doc.studyPlan) {
                 doc.studyPlan = {};
             }
             doc.studyPlan.defaultLesson = matches[0];
-            doc.landingUrl='/studyplan';
+            doc.landingUrl = '/studyplan';
             return doc;
         }
     }
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 let express = require('express');
 let path = require('path');
 let parse = require('./parse');
+let fs = require('fs');
 
 let app = express();
-let productConfigFolder = process.cwd() + '/ATOM.WEB';
+let productConfigFolder = process.cwd() + '/prod/ATOM.WEB';
 
 app.set('port', (process.env.PORT || 3000));
 app.use('/set', function (req, res, send) {
@@ -11,6 +12,22 @@ app.use('/set', function (req, res, send) {
     res.send(val);
 });
 
+app.use('/list', function (req, res, send) {
+    let files = parse.list(productConfigFolder);
+    res.send(files.join('<br/>'));
+});
+app.use('/current', function (req, res, send) {
+    let data = parse.read(productConfigFolder);
+    res.write(data);
+    fs.writeFileSync('current.csv', data);
+    res.end();
+});
+app.use('/v2', function (req, res, send) {
+    let data = parse.v2(productConfigFolder);
+    res.write(data);
+    fs.writeFileSync('v2.csv', data);
+    res.end();
+});
 app.listen(app.get('port'), () => {
     console.log('app running on port ' + app.get('port'));
 });
